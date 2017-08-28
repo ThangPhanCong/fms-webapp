bf3f3c393175b36239cfe3c216bc61454b03dc68
minhnt58 <tienminh.uet@gmail.com>
2017-08-28 11:05:40 +0700

    refactor

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ let app = express();
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile('/build/index.html');
+});
+
 app.listen(3000, function (err) {
   if (err) return console.log(err);
 
-  console.log('Server is up on port ' + 3000);
+  console.log('Fms-webapp server is up on port ' + 3000);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ],
   output: {
     path: __dirname,
-    filename: './build/bundle.js'
+    filename: './public/bundle.js'
   },
   resolve: {
     root: __dirname,
