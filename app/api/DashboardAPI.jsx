1f164283dcf18947a00dec7a0710f32ec2699fb6
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-08-31 18:18:19 +0700

    ok

--- app/api/DashboardAPI.jsx
+++ app/api/DashboardAPI.jsx
@@ -6,16 +6,82 @@ let conversations = [
         name: "Nguyễn Đăng Thế",
         messages: [
             {   
-                message: "ok men",
+                message: "ok men ok men ok men ok men ok men ok men ok men ok men ok men",
                 sender: {
                     fb_id: '1958682827749688',// user_id,
                     name: "Bùi Mạnh Thắng"
                 }
             }, {
-                message: "ok men em",
+                message: "viet nam iet nam viet nam viet nam viet",
                 sender: {
                     fb_id: '100004430053999', // user_id,
                     name: "Nguyễn Đăng Thế"
+                }
+            }, {
+                message: "thai binh thai binh thai binh",
+                sender: {
+                    fb_id: '100004430053999', // user_id,
+                    name: "Nguyễn Đăng Thế"
+                }
+            }, {   
+                message: "binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh finh",
+                sender: {
+                    fb_id: '1958682827749688',// user_id,
+                    name: "Bùi Mạnh Thắng"
+                }
+            }, {   
+                message: "uet uet uet uet uet uet",
+                sender: {
+                    fb_id: '1958682827749688',// user_id,
+                    name: "Bùi Mạnh Thắng"
+                }
+            },{
+                message: "viet nam iet nam viet nam viet nam viet",
+                sender: {
+                    fb_id: '100004430053999', // user_id,
+                    name: "Nguyễn Đăng Thế"
+                }
+            }, {
+                message: "thai binh thai binh thai binh",
+                sender: {
+                    fb_id: '100004430053999', // user_id,
+                    name: "Nguyễn Đăng Thế"
+                }
+            }, {   
+                message: "binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh finh",
+                sender: {
+                    fb_id: '1958682827749688',// user_id,
+                    name: "Bùi Mạnh Thắng"
+                }
+            }, {   
+                message: "uet uet uet uet uet uet",
+                sender: {
+                    fb_id: '1958682827749688',// user_id,
+                    name: "Bùi Mạnh Thắng"
+                }
+            },{
+                message: "viet nam iet nam viet nam viet nam viet",
+                sender: {
+                    fb_id: '100004430053999', // user_id,
+                    name: "Nguyễn Đăng Thế"
+                }
+            }, {
+                message: "thai binh thai binh thai binh",
+                sender: {
+                    fb_id: '100004430053999', // user_id,
+                    name: "Nguyễn Đăng Thế"
+                }
+            }, {   
+                message: "binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh dinh binh finh",
+                sender: {
+                    fb_id: '1958682827749688',// user_id,
+                    name: "Bùi Mạnh Thắng"
+                }
+            }, {   
+                message: "uet uet uet uet uet uet",
+                sender: {
+                    fb_id: '1958682827749688',// user_id,
+                    name: "Bùi Mạnh Thắng"
                 }
             }
         ]
