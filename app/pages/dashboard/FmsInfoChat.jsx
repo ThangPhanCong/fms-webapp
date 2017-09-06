35b28c1e33fd60342e8914e91f8f9f5c4c287492
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-09-06 09:27:29 +0700

    update view message form

---
+++ app/pages/dashboard/FmsInfoChat.jsx
@@ -0,0 +1,14 @@
+const React = require('react');
+
+let FmsInfoChat = React.createClass({
+  render: function () {
+    return (
+      <div> 
+        <p></p>
+        <p></p>
+      </div>
+    );
+  }
+});
+
+module.exports = FmsInfoChat;
