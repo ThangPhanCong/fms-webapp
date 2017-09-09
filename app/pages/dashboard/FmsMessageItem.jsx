e68c3d575dd11d9bb492e613a60a828e552718ea
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-09-09 20:30:43 +0700

    small update for message item

--- app/pages/dashboard/FmsMessageItem.jsx
+++ app/pages/dashboard/FmsMessageItem.jsx
@@ -1,15 +1,20 @@
 'use strict';
 
 const React = require('react');
+const rightArrowImg = require('right_arrow.png');
+const leftArrowImg = require('left_arrow.png');
 
 let FmsTextMessageContent = require('FmsTextMessageContent');
 
 let FmsMessageItem = React.createClass({
 	render: function () {
 		let avaUrl = `https://graph.facebook.com/v2.10/${this.props.message.from.id}/picture`;
-    let messageWrapper = (this.props.isSelf) ? " right-message-wrapper" : " left-message-wrapper";
-		let profileWrapper = (this.props.isSelf) ? " right-profile-wrapper" : " left-profile-wrapper";
-		let messageContent = (this.props.isSelf) ? " right-message-content" : " left-message-content";
+		let isSelf = this.props.isSelf;
+    let messageWrapper = (isSelf) ? " right-message-wrapper" : " left-message-wrapper";
+		let profileWrapper = (isSelf) ? " right-profile-wrapper" : " left-profile-wrapper";
+		let messageContent = (isSelf) ? " right-message-content" : " left-message-content";
+		let srcArrow = (isSelf) ? rightArrowImg : leftArrowImg;
+		let arrow = (isSelf) ? " right-arrow-message" : " left-arrow-message";
 		let isLast = (this.props.isLast) ? " last-message" : "";
 		return (
 			<div className="message-item">
@@ -17,6 +22,7 @@ let FmsMessageItem = React.createClass({
           <div className={"profile-wrapper" + profileWrapper}>
 					  <img src={avaUrl} className="profile-message" />
           </div>
+					<img src={srcArrow} className={arrow}/>
           <div className={"message-content" + messageContent}>
 					  <FmsTextMessageContent content={this.props.message.message}/>
           </div>
