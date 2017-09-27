1d9992d4aaebadce1f8c3eb0d07ad0390a252b0c
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-09-27 11:38:33 +0700

    update seen time for conversation, fix attachment loading problem.

--- app/pages/dashboard/conversation-area/FmsAttachmentContent.jsx
+++ app/pages/dashboard/conversation-area/FmsAttachmentContent.jsx
@@ -1,11 +1,21 @@
 const React = require('react');
+const unavailableImg = require('unavailable.png');
 
 let FmsAttachmentContent = React.createClass({
+  getInitialState: function () {
+    return {
+      isError: false
+    }
+  },
   getDefaultProps: function () {
     return {
       isSelf: false,
       hasMessage: -1
     };
+  },
+  handleLoadError: function () {
+    this.setState({ isError: true });
+    this.props.attachmentLoadDone();
   },
   render: function () {
     let self = this;
@@ -18,14 +28,14 @@ let FmsAttachmentContent = React.createClass({
     } else {
       hasMessage = " no-message";
     }
-    let preview = self.props.preview;
-    let msgAttachWrapper = (preview == "" || preview == null || preview == undefined) ? " hide" : "";
+    let preview = (this.state.isError == false) ? self.props.preview : unavailableImg;
+    let msgAttachWrapper = (self.props.preview == "" || self.props.preview == null || self.props.preview == undefined) ? " hide" : "";
 
     return (
       <div className={"message-attachment-wrapper" + hasMessage + msgAttachWrapper}>
         <a href={self.props.origin} target="_blank">
-          <img className={messageAttachment + ' ' + self.props.type} src={self.props.preview} 
-          onLoad={this.props.attachmentLoadDone}/>
+          <img className={messageAttachment + ' ' + self.props.type} src={preview} 
+          onLoad={this.props.attachmentLoadDone} onError={this.handleLoadError}/>
         </a>
       </div>
     )
