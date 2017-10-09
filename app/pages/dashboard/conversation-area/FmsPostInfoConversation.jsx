b87c2aa5614df76ee2c7e2fa9744be7ef2f3a313
Bùi Mạnh Thắng <buimanhthangnt@gmail.com>
2017-10-09 19:29:01 +0700

    okkkk!

--- app/pages/dashboard/conversation-area/FmsPostInfoConversation.jsx
+++ app/pages/dashboard/conversation-area/FmsPostInfoConversation.jsx
@@ -1,10 +1,26 @@
 const React = require('react');
+const uuid = require('uuid');
 
 let FmsPostInfoConversation = React.createClass({
+  renderAttachments: (attachments) => {
+    let count = 0;
+    console.log(attachments);
+    let atts =  attachments.map(att => {
+      count++;
+      let style = {
+        width: 50 + "%",
+        borderRadius: 5 + "px"
+      }
+      return <a href={att.url} target="_blank" key={uuid()}><img src={att.media.image.src} style={style}/></a>
+    });
+    return atts;
+  },
   render: function () {
+    let attachments = (this.props.content.attachments) ? this.props.content.attachments[0].data[0].subattachments.data : [];
     return (
       <div className="post-info-conversation">
         <p>{this.props.content.message}</p>
+        {this.renderAttachments(attachments)}
       </div>
     )
   }
