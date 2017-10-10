1a1a02de152c96f2ae954419bf3f0594235e42d3
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-10-10 13:44:11 +0700

    attachments in conversation

--- app/pages/dashboard/conversation-area/FmsPostInfoConversation.jsx
+++ app/pages/dashboard/conversation-area/FmsPostInfoConversation.jsx
@@ -3,24 +3,40 @@ const uuid = require('uuid');
 
 let FmsPostInfoConversation = React.createClass({
   renderAttachments: (attachments) => {
-    let count = 0;
-    console.log(attachments);
-    let atts =  attachments.map(att => {
-      count++;
+    let atts, type;
+    if (attachments && Array.isArray(attachments) && attachments.length == 1) {
+      let short = attachments[0];
+      if (short.photos && Array.isArray(short.photos)) {
+        atts = short.photos;
+        type = 1;
+      } else if (Array.isArray(short.data) && short.data.length == 1 && short.data[0].subattachments 
+                 && Array.isArray(short.data[0].subattachments.data)) {
+        atts = short.data[0].subattachments.data;
+        type = 2;
+      } else {
+        atts = [];
+        type = 3;
+      }
+    }
+    return atts.map(att => {
+      let src;
+      if (type == 1) {
+        src = att;
+      } else if (type == 2) {
+        src = att.media.image.src;
+      }
       let style = {
-        width: 50 + "%",
+        height: 150 + "px",
         borderRadius: 5 + "px"
       }
-      return <a href={att.url} target="_blank" key={uuid()}><img src={att.media.image.src} style={style}/></a>
+      return <a className="attachment-in-conversation" href={src} target="_blank" key={uuid()}><img src={src} style={style}/></a>
     });
-    return atts;
   },
   render: function () {
-    let attachments = (this.props.content.attachments) ? this.props.content.attachments[0].data[0].subattachments.data : [];
     return (
       <div className="post-info-conversation">
         <p>{this.props.content.message}</p>
-        {this.renderAttachments(attachments)}
+        {this.renderAttachments(this.props.content.attachments)}
       </div>
     )
   }
