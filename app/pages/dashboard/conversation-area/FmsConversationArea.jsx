44002898b1e05db88869ffdc894d2880b4133cf1
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-10-12 19:43:04 +0700

    minor change

--- app/pages/dashboard/conversation-area/FmsConversationArea.jsx
+++ app/pages/dashboard/conversation-area/FmsConversationArea.jsx
@@ -83,7 +83,7 @@ let FmsConversationArea = React.createClass({
 	},
 	componentDidUpdate: function (prevProp, prevState) {
 		var list = ReactDOM.findDOMNode(this.refs.chat_area);
-		list.scrollTop = list.scrollHeight - lastScrollPosition - 51;
+		list.scrollTop = list.scrollHeight - lastScrollPosition;
 		if (!this.state.postInfo && list.clientHeight + 12 > list.scrollHeight) {
 			this.loadMoreMessages();
 		}
