6b36ae5140811e54934604dcc5b6244a7de78296
minhnt58 <tienminh.uet@gmail.com>
2017-11-09 13:42:04 +0700

    fix exact

--- app/pages/settings/FmsSettings.jsx
+++ app/pages/settings/FmsSettings.jsx
@@ -30,15 +30,15 @@ class FmsSettings extends React.Component {
         </Col>
         <Col xs={12} sm={4}>
           <Switch>
-            <Route exact="exact" path="/projects/:project_alias/settings/general" component={FmsGeneral}/>
-            <Route exact="exact" path="/projects/:project_alias/settings/tag" component={FmsTag}/>
-            <Route exact="exact" path="/projects/:project_alias/settings/page" component={FmsPage}/>
-            <Route exact="exact" path="/projects/:project_alias/settings/post" component={FmsPost}/>
-            <Route exact="exact" path="/projects/:project_alias/settings/customer" component={FmsCustomer}/>
-            <Route exact="exact" path="/projects/:project_alias/settings/answer" component={FmsAnswer}/>
-            <Route exact="exact" path="/projects/:project_alias/settings/employ" component={FmsEmploy}/>
-            <Route exact="exact" path="/projects/:project_alias/settings/noti" component={FmsNoti}/>
-            <Route exact="exact" path="/projects/:project_alias/settings/pay" component={FmsPay}/>
+            <Route path="/projects/:project_alias/settings/general" component={FmsGeneral}/>
+            <Route path="/projects/:project_alias/settings/tag" component={FmsTag}/>
+            <Route path="/projects/:project_alias/settings/page" component={FmsPage}/>
+            <Route path="/projects/:project_alias/settings/post" component={FmsPost}/>
+            <Route path="/projects/:project_alias/settings/customer" component={FmsCustomer}/>
+            <Route path="/projects/:project_alias/settings/answer" component={FmsAnswer}/>
+            <Route path="/projects/:project_alias/settings/employ" component={FmsEmploy}/>
+            <Route path="/projects/:project_alias/settings/noti" component={FmsNoti}/>
+            <Route path="/projects/:project_alias/settings/pay" component={FmsPay}/>
           </Switch>
         </Col>
       </Row>
