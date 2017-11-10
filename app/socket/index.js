e3925c3f33a0bbb3fe5c81bf3ab176c98e47950a
minhnt58 <tienminh.uet@gmail.com>
2017-11-10 17:39:34 +0700

    ok

--- app/socket/index.js
+++ app/socket/index.js
@@ -15,8 +15,8 @@ export const subscribePageChanges = ({page_fb_id, onUpdateChanges}) => {
 
 export const subscribeProjectChanges = ({project_alias, onUpdateChanges}) => {
   if (socket) {
-    socket.emit(event.SUBSCRIBE_PROJECTS_CHANGES_EVENT, project_alias);
     socket.on(event.PROJECTS_CHANGES_EVENT, onUpdateChanges);
+    socket.emit(event.SUBSCRIBE_PROJECTS_CHANGES_EVENT, project_alias);
   } else {
     retry(subscribeProjectChanges, {project_alias, onUpdateChanges});
   }
@@ -24,6 +24,7 @@ export const subscribeProjectChanges = ({project_alias, onUpdateChanges}) => {
 
 export const unSubscribeProjectChanges = ({project_alias}) => {
   if (socket) {
+    socket.off(event.PROJECTS_CHANGES_EVENT);
     socket.emit(event.UN_SUBSCRIBE_PROJECTS_CHANGES_EVENT, project_alias);
   } else {
     retry(unSubscribeProjectChanges, {project_alias});
