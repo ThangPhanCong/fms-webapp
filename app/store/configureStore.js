2936543a0bd5697e4ebc82707863aff925771de2
minhnt58 <tienminh.uet@gmail.com>
2017-11-15 16:01:38 +0700

    ok

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -4,10 +4,16 @@ import thunk from 'redux-thunk';
 import reducers from '../reducers/index';
 
 export const configure = (initialState = {}) => {
-  const store = redux.createStore(reducers, initialState, redux.compose(
-    redux.applyMiddleware(thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-  ));
+  let middlewareWrapper;
+  if (process.env.NODE_ENV !== 'production') {
+    middlewareWrapper = redux.compose(
+      redux.applyMiddleware(thunk),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+  } else {
+    middlewareWrapper = redux.compose(redux.applyMiddleware(thunk))
+  }
+  const store = redux.createStore(reducers, initialState, middlewareWrapper);
 
   return store;
 };
