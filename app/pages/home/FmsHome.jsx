2e55468d31274367048a8f9c72ab2ea0bbeaaaa4
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-11-16 14:12:54 +0700

    homepage

--- app/pages/home/FmsHome.jsx
+++ app/pages/home/FmsHome.jsx
@@ -4,24 +4,20 @@ import {Link, Redirect, withRouter} from 'react-router-dom';
 import * as store from '../../helpers/storage';
 import {logIn} from '../../actions/auth';
 
+import homepageImg from '../../images/homepage.jpg';
+
 class FmsHome extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-
-  }
-
   onClickLoginBtn() {
     const {dispatch} = this.props;
     dispatch(logIn());
   }
 
   render() {
+    let style = {
+      background: `url(${homepageImg})`
+    }
     return (
-      <div className="container-fluid homepage page">
+      <div className="container-fluid homepage page" style={style}>
         <h1 className="page-title">Facebook Marketing Suite</h1>
         <p className="page-description">Tools for conversation management, customers, selling at stall, integrated transportation and supporting utilities</p>
 
