6fe183a09d8e47401ac22bff80958395efecee07
minhnt58 <tienminh.uet@gmail.com>
2017-11-16 17:01:28 +0700

    ok

---
+++ app/actions/page.js
@@ -0,0 +1,26 @@
+import * as store from '../helpers/storage';
+import pageApi from '../api/PagesApi';
+
+export const PAGES_LOADING = 'PAGES_LOADING';
+export const PAGES_LOADED = 'PAGES_LOADED';
+
+export const getPages = () => dispatch => {
+  dispatch({
+    type: PAGES_LOADING
+  })
+
+  pageApi.getPages()
+    .then(pages => {
+      dispatch({
+        type: PAGES_LOADED,
+        pages
+      })
+    })
+    .catch(err => {
+      dispatch({
+        type: PAGES_LOADED,
+        pages: []
+      })
+      console.log(err);
+    })
+}
