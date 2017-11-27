6cfda0fd89709778bfc5240c4ba9a5b974649dce
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-11-28 00:03:20 +0700

    attachment in post

--- app/pages/dashboard/selectedConversation/FmsPostInfoConversation.jsx
+++ app/pages/dashboard/selectedConversation/FmsPostInfoConversation.jsx
@@ -4,22 +4,13 @@ import { connect } from 'react-redux';
 
 class FmsPostInfoConversation extends React.Component {
   renderAttachments(attachments) {
-    let atts = [], type = 3;
-    if (attachments && Array.isArray(attachments) && attachments.length == 1) {
-      let short = attachments[0];
-      if (short.photos && Array.isArray(short.photos)) {
-        atts = short.photos;
-        type = 1;
-      } else if (Array.isArray(short.data) && short.data.length == 1 && short.data[0].subattachments
-        && Array.isArray(short.data[0].subattachments.data)) {
-        atts = short.data[0].subattachments.data;
-        type = 2;
-      }
+    let atts = [];
+    if (attachments && Array.isArray(attachments.data)) {
+      let short = attachments.data[0];
+      if (short.subattachments && Array.isArray(short.subattachments.data)) atts = short.subattachments.data;
     }
     return atts.map(att => {
-      let src;
-      if (type == 1) src = att;
-      else if (type == 2) src = att.media.image.src;
+      let src = att.media.image.src;
       return <a className="attachment-in-conversation" href={src} target="_blank" key={uuid()}>
         <img className="image-in-conversation" src={src} /></a>
     });
