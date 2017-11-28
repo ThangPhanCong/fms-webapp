120a64c0db321a9bf39e9765bdd15251ac1553f7
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2017-11-28 13:38:23 +0700

    refresh attachment inpost

--- app/pages/dashboard/selectedConversation/FmsPostInfoConversation.jsx
+++ app/pages/dashboard/selectedConversation/FmsPostInfoConversation.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import uuid from 'uuid';
 import { connect } from 'react-redux';
+import DashboardApi from '../../../api/DashboardApi';
+import { setPostInfo } from '../../../actions/dashboard/chat/messages';
 
 class FmsPostInfoConversation extends React.Component {
+  handleAttachExpired() {
+    DashboardApi.updateExpiredAttachment("post", this.props.postInfo._id)
+    .then(res => {
+      this.props.dispatch(setPostInfo(res));
+    }, err => {
+      console.log(err);
+    });
+  }
   renderAttachments(attachments) {
+    let self = this;
     let atts = [];
     if (attachments && Array.isArray(attachments.data)) {
       let short = attachments.data[0];
@@ -12,7 +23,7 @@ class FmsPostInfoConversation extends React.Component {
     return atts.map(att => {
       let src = att.media.image.src;
       return <a className="attachment-in-conversation" href={src} target="_blank" key={uuid()}>
-        <img className="image-in-conversation" src={src} /></a>
+        <img className="image-in-conversation" src={src} onError={this.handleAttachExpired.bind(this)}/></a>
     });
   }
   renderPost() {
