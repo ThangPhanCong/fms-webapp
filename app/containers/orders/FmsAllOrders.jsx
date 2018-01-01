e72df7ee04c6fe654022b2565cb121d89d25fe11
minhnt58 <tienminh.uet@gmail.com>
2018-01-01 23:52:05 +0700

    minor

--- app/containers/orders/FmsAllOrders.jsx
+++ app/containers/orders/FmsAllOrders.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import FmsPageTitle from "../../commons/page-title/FmsPageTitle";
+import AllOrderBody from "./all-orders/AllOrderBody";
 
 class FmsAllOrders extends Component {
     render() {
@@ -14,21 +15,7 @@ class FmsAllOrders extends Component {
             [
                 <FmsPageTitle key={1} title="Tất cả đơn hàng" route={`${projectName}/Quản lí đơn hàng/Tất cả đơn hàng`}/>,
 
-                <div key={2} className="wrapper wrapper-content animated fadeInRight">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="text-center m-t-lg">
-                                <h1>
-                                    Tất cả đơn hàng
-                                </h1>
-                                <small>
-                                    It is an application skeleton for a typical web app. You can use it to quickly bootstrap
-                                    your webapp projects.
-                                </small>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <AllOrderBody key={2} />
             ]
         )
     }
---
+++ app/containers/orders/all-orders/AllOrderBody.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+class AllOrderBody extends Component {
+    render () {
+        return (
+            <div className="wrapper wrapper-content animated fadeInRight">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="text-center m-t-lg">
+                            <h1>
+                                Tất cả đơn hàng
+                            </h1>
+                            <small>
+                                It is an application skeleton for a typical web app. You can use it to quickly bootstrap
+                                your webapp projects.
+                            </small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default AllOrderBody;
