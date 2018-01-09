eb118dd595ee726ff30d1718f8cf77efd618a26f
BUIMANHTHANG\thangbm <buimanhthangnt@gmail.com>
2018-01-10 00:09:56 +0700

    ui change posts

--- app/containers/posts/FmsPostItem/FmsPostItem.jsx
+++ app/containers/posts/FmsPostItem/FmsPostItem.jsx
@@ -1,48 +1,64 @@
 import React from 'react';
 
 import {Checkbox} from 'react-bootstrap';
-import uuid from 'uuid';
 import FmsCroppedImage from '../../../commons/FmsCroppedImage/FmsCroppedImage';
 import FmsScrollableDiv from '../../../commons/scroll-bar/FmsScrollableDiv';
+import FmsDate from '../../../helpers/FmsDate';
 
 class FmsPostItem extends React.Component {
-  onToggleChange() {
-    this.props.onToggleChange(this.props.data._id);
-  }
-
-  renderImgs() {
-    let {attachments} = this.props.data;
-    if (attachments && Array.isArray(attachments) && attachments.length > 0) {
-      if (Array.isArray(attachments[0].data)) {
-        return attachments[0].data.map((a, i) => {
-          return <FmsCroppedImage className="image" key={i} src={a.preview || a.src}/>;
-        });
-      }
+    onToggleChange() {
+        this.props.onToggleChange(this.props.data._id);
     }
-  }
 
-  render() {
-    let {page, message, hide_comment} = this.props.data;
+    getCreatedTime() {
+        let date = new FmsDate(this.props.data.created_time);
+        return date.getTimePostItem();
+    }
 
-    return (
-      <div className="post-item">
-        <div className="page-info">
-          <div className="page-name">{page.name}</div>
-        </div>
-        <FmsScrollableDiv className="content-wrapper">
-          <p className="content">{message}</p>
-          <div className="image-wrapper">
-            {this.renderImgs()}
-          </div>
-        </FmsScrollableDiv>
-        <div>
-          <Checkbox type="checkbox"
-                    checked={hide_comment}
-                    onChange={this.onToggleChange.bind(this)}> Ẩn bình luận</Checkbox>
-        </div>
-      </div>
-    );
-  }
+    renderImgs() {
+        let {attachments} = this.props.data;
+        if (attachments && Array.isArray(attachments) && attachments.length > 0) {
+            if (Array.isArray(attachments[0].data)) {
+                return attachments[0].data.map((a, i) => {
+                    return <FmsCroppedImage className="image" key={i} src={a.preview || a.src}/>;
+                });
+            }
+        }
+    }
+
+    render() {
+        let {page, message, hide_comment} = this.props.data;
+        let avaUrl = `https://graph.facebook.com/v2.10/${page.fb_id}/picture`;
+        let pageFb = `https://facebook.com/${page.fb_id}`;
+        let attachments = (this.props.data.attachments) ? "" : " hide";
+
+        return (
+            <div className="post-item">
+                <div className="page-info">
+                    <div className="avatar-wrapper">
+                        <a href={pageFb} target="_blank">
+                            <img className="avatar" src={avaUrl}/>
+                        </a>
+                    </div>
+                    <div className="post-info">
+                        <div className="page-name">{page.name}</div>
+                        <div className="created-time">{this.getCreatedTime()}</div>
+                    </div>
+                </div>
+                <FmsScrollableDiv className="content-wrapper">
+                    <p className="content">{message}</p>
+                    <div className={"image-wrapper" + attachments}>
+                        {this.renderImgs()}
+                    </div>
+                </FmsScrollableDiv>
+                <div>
+                    <Checkbox type="checkbox"
+                              checked={hide_comment}
+                              onChange={this.onToggleChange.bind(this)}> Ẩn bình luận</Checkbox>
+                </div>
+            </div>
+        );
+    }
 }
 
 module.exports = FmsPostItem;
--- app/helpers/FmsDate.js
+++ app/helpers/FmsDate.js
@@ -1,87 +1,97 @@
 export default class FmsDate {
-  constructor(_date) {
-    let current = new Date();
-    let date;
-    if (!_date) date = current;
-    else date = new Date(_date);
+    constructor(_date) {
+        let current = new Date();
+        let date;
+        if (!_date) date = current;
+        else date = new Date(_date);
 
-    let cday = current.getDate();
-    if (cday < 10) cday = "0" + cday;
-    let cmonth = current.getMonth() + 1;
-    if (cmonth < 10) cmonth = "0" + cmonth;
-    let cyear = current.getFullYear();
+        let cday = current.getDate();
+        if (cday < 10) cday = "0" + cday;
+        let cmonth = current.getMonth() + 1;
+        if (cmonth < 10) cmonth = "0" + cmonth;
+        let cyear = current.getFullYear();
 
-    let day = date.getDate();
-    if (day < 10) day = "0" + day;
-    let month = date.getMonth() + 1;
-    if (month < 10) month = "0" + month;
-    let year = date.getFullYear();
-    let hour = date.getHours();
-    if (hour < 10) hour = "0" + hour;
-    let minute = date.getMinutes();
-    if (minute < 10) minute = "0" + minute;
+        let day = date.getDate();
+        if (day < 10) day = "0" + day;
+        let month = date.getMonth() + 1;
+        if (month < 10) month = "0" + month;
+        let year = date.getFullYear();
+        let hour = date.getHours();
+        if (hour < 10) hour = "0" + hour;
+        let minute = date.getMinutes();
+        if (minute < 10) minute = "0" + minute;
 
-    let whatday;
-    if (cyear === year && cmonth === month && cday === day) whatday = "Hôm nay";
-    else if (cyear === year && cmonth === month && cday - day === 1) whatday = "Hôm qua";
-    else whatday = "Ngày " + day + " tháng " + month;
+        let whatday;
+        if (cyear === year && cmonth === month && cday === day) whatday = "Hôm nay";
+        else if (cyear === year && cmonth === month && cday - day === 1) whatday = "Hôm qua";
+        else whatday = "Ngày " + day + " tháng " + month;
 
-    let dayInWeek;
-    switch (date.getDay()) {
-      case 0:
-        dayInWeek = "CNhật";
-        break;
-      case 1:
-        dayInWeek = "Thứ 2";
-        break;
-      case 2:
-        dayInWeek = "Thứ 3";
-        break;
-      case 3:
-        dayInWeek = "Thứ 4";
-        break;
-      case 4:
-        dayInWeek = "Thứ 5";
-        break;
-      case 5:
-        dayInWeek = "Thứ 6";
-        break;
-      case 6:
-        dayInWeek = "Thứ 7";
-        break;
+        let dayInWeek;
+        switch (date.getDay()) {
+            case 0:
+                dayInWeek = "CNhật";
+                break;
+            case 1:
+                dayInWeek = "Thứ 2";
+                break;
+            case 2:
+                dayInWeek = "Thứ 3";
+                break;
+            case 3:
+                dayInWeek = "Thứ 4";
+                break;
+            case 4:
+                dayInWeek = "Thứ 5";
+                break;
+            case 5:
+                dayInWeek = "Thứ 6";
+                break;
+            case 6:
+                dayInWeek = "Thứ 7";
+                break;
+        }
+        this.date = day;
+        this.month = month;
+        this.year = year;
+        this.whatday = whatday;
+        this.day = dayInWeek;
+        this.hour = hour;
+        this.minute = minute;
+        this.different = cday - day;
+        this.getTimeChatArea = this.getTimeChatArea.bind(this);
     }
-    this.date = day;
-    this.month = month;
-    this.year = year;
-    this.whatday = whatday;
-    this.day = dayInWeek;
-    this.hour = hour;
-    this.minute = minute;
-    this.different = cday - day;
-    this.getTimeChatArea = this.getTimeChatArea.bind(this);
-  }
 
-  getTimeChatArea() {
-    return this.whatday;
-  }
-
-  getTimeConversationItem() {
-    if (this.different === 0) return this.hour + ":" + this.minute;
-    else if (this.different < 7 && this.different > 0) return this.day;
-    else return this.date + "/" + this.month;
-  }
-
-  getTimeMessageItem() {
-    return this.hour + ":" + this.minute;
-  }
-
-  getTimeInfoChat() {
-    let res = "Đã xem gần nhất";
-    let whatday = this.whatday;
-    if (this.whatday !== " hôm nay" && this.whatday !== " hôm qua") {
-      whatday = " ngày " + this.date + "/" + this.month;
+    getTimeChatArea() {
+        return this.whatday;
     }
-    let moment = " lúc " + this.hour + ":" + this.minute;
-    return res + moment + whatday;
-  }
+
+    getTimeConversationItem() {
+        if (this.different === 0) return this.hour + ":" + this.minute;
+        else if (this.different < 7 && this.different > 0) return this.day;
+        else return this.date + "/" + this.month;
+    }
+
+    getTimeMessageItem() {
+        return this.hour + ":" + this.minute;
+    }
+
+    getTimeInfoChat() {
+        let res = "Đã xem gần nhất";
+        let whatday = this.whatday;
+        if (this.whatday !== " hôm nay" && this.whatday !== " hôm qua") {
+            whatday = " ngày " + this.date + "/" + this.month;
+        }
+        let moment = " lúc " + this.hour + ":" + this.minute;
+        return res + moment + whatday;
+    }
+
+    getTimePostItem() {
+        let res = "Đăng";
+        let whatday = this.whatday;
+        if (this.whatday !== " hôm nay" && this.whatday !== " hôm qua") {
+            whatday = " ngày " + this.date + "/" + this.month;
+        }
+        let moment = " lúc " + this.hour + ":" + this.minute;
+        return res + moment + whatday;
+    }
 }
