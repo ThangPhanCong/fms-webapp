57d2a9741376e4d8c80851fa488cca5f5dcd86f3
minhnt58 <tienminh.uet@gmail.com>
2018-01-19 17:53:53 +0700

    ok

--- app/containers/orders/FmsAllOrders.jsx
+++ app/containers/orders/FmsAllOrders.jsx
@@ -11,6 +11,8 @@ class FmsAllOrders extends Component {
             projectName = project.name;
         }
 
+        console.log('FmsAllOrders project', project);
+
         return (
             <Fragment>
                 <FmsPageTitle title="Tất cả đơn hàng" route={`${projectName}/Quản lí đơn hàng/Tất cả đơn hàng`}/>
