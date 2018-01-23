b80e3028268551f3e820a8f647d858af5b58faeb
nguyentd1501 <nguyen.td1501@gmail.com>
2018-01-23 11:43:37 +0700

    fix Search bar

--- app/containers/orders/all-orders/FmsNewOrderSearchBar.jsx
+++ app/containers/orders/all-orders/FmsNewOrderSearchBar.jsx
@@ -80,76 +80,71 @@ class FmsNewOrderSearchBar extends Component {
         const {orderTags} = this.state;
 
         return (
-            <div>
-                <div className="row">
-                    <div className="col-sm-3 pull-right">
-                        <div className="form-group">
-                            <label className="control-label">Ngày tạo</label>
-                            <FmsDatePicker onEvent={this.handleEvent.bind(this)}/>
-                        </div>
+            <div className='row'>
+                <div className="col-sm-2">
+                    <div className="form-group">
+                        <label className="control-label">Mã đơn</label>
+                        <input type="text" className="form-control"
+                            ref='id'
+                            value={id || ''}
+                            onChange={() => this.onChangeInput('id')}
+                        />
                     </div>
                 </div>
-                <div className='row'>
-                    <div className="col-sm-2">
-                        <div className="form-group">
-                            <label className="control-label">Mã đơn</label>
-                            <input type="text" className="form-control"
-                                ref='id'
-                                value={id || ''}
-                                onChange={() => this.onChangeInput('id')}
-                            />
-                        </div>
+                <div className="col-sm-2">
+                    <div className="form-group">
+                        <label className="control-label">Khách hàng</label>
+                        <input type="text" className="form-control"
+                            ref='customer_name'
+                            value={customer_name || ''}
+                            onChange={() => this.onChangeInput('customer_name')}
+                        />
                     </div>
-                    <div className="col-sm-2">
-                        <div className="form-group">
-                            <label className="control-label">Khách hàng</label>
-                            <input type="text" className="form-control"
-                                ref='customer_name'
-                                value={customer_name || ''}
-                                onChange={() => this.onChangeInput('customer_name')}
-                            />
-                        </div>
+                </div>
+                <div className="col-sm-2">
+                    <div className="form-group">
+                        <label className="control-label">Điện thoại</label>
+                        <input type="text" className="form-control"
+                            ref='customer_phone'
+                            value={customer_phone || ''}
+                            onChange={() => this.onChangeInput('customer_phone')}
+                        />
                     </div>
-                    <div className="col-sm-2">
-                        <div className="form-group">
-                            <label className="control-label">Điện thoại</label>
-                            <input type="text" className="form-control"
-                                ref='customer_phone'
-                                value={customer_phone || ''}
-                                onChange={() => this.onChangeInput('customer_phone')}
-                            />
-                        </div>
+                </div>
+                <div className="col-sm-2">
+                    <div className="form-group">
+                        <label className="control-label">Sản phẩm</label>
+                        <input type="text" className="form-control"
+                            ref='product'
+                            value={product || ''}
+                            onChange={() => this.onChangeInput('product')}
+                        />
                     </div>
-                    <div className="col-sm-3">
-                        <div className="form-group">
-                            <label className="control-label">Sản phẩm</label>
-                            <input type="text" className="form-control"
-                                ref='product'
-                                value={product || ''}
-                                onChange={() => this.onChangeInput('product')}
-                            />
-                        </div>
+                </div>
+                <div className="col-sm-2">
+                    <div className="form-group">
+                        <label className="control-label">Ngày tạo</label>
+                        <FmsDatePicker onEvent={this.handleEvent.bind(this)}/>
                     </div>
-                    
-                    <div className="col-sm-3">
-                        <div className="form-group">
-                            <label className="control-label">Đánh dấu</label>
-                            <select className="form-control"
-                                    ref='order_tag'
-                                    value={order_tag || ''}
-                                    onChange={() => {
-                                        this.onChangeInput('order_tag')
-                                    }}
-                            >
-                                {
-                                    orderTags.map(
-                                        (tag, i) => (
-                                            <option key={i} value={tag._id}>{tag.name}</option>
-                                        )
+                </div>
+                <div className="col-sm-2">
+                    <div className="form-group">
+                        <label className="control-label">Đánh dấu</label>
+                        <select className="form-control"
+                                ref='order_tag'
+                                value={order_tag || ''}
+                                onChange={() => {
+                                    this.onChangeInput('order_tag')
+                                }}
+                        >
+                            {
+                                orderTags.map(
+                                    (tag, i) => (
+                                        <option key={i} value={tag._id}>{tag.name}</option>
                                     )
-                                }
-                            </select>
-                        </div>
+                                )
+                            }
+                        </select>
                     </div>
                 </div>
             </div>
