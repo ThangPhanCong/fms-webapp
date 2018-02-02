927a37abfe19a70f221269f8e02ff589d36b04ce
nguyentd1501 <nguyen.td1501@gmail.com>
2018-02-02 17:47:00 +0700

    add api

--- app/api/StaffApi.js
+++ app/api/StaffApi.js
@@ -7,3 +7,25 @@ export function countStaffs(project_id, filter = {}) {
 
     return get(`/api/a/staffs/count?${queryParams}`, getByProjectId(project_id));
 }
+
+export function getStaffs(project_id, filter = {}) {
+    const queryParams = toQueryParams(filter);
+
+    return get(`/api/a/staffs?${queryParams}`, getByProjectId(project_id));
+}
+
+export function createNewStaff(project_id, staff) {
+    const payload = {...staff};
+
+    return post(`/api/a/staffs`, payload, getByProjectId(project_id));
+}
+
+export function updateStaff(project_id, staff) {
+    const payload = {...staff};
+
+    return put(`/api/a/staffs/${staff._id}`, payload, getByProjectId(project_id));
+}
+
+export function deleteStaff(project_id, staff_id) {
+    return apiSender.delete(`/api/a/staffs/${staff_id}`, getByProjectId(project_id));
+}
