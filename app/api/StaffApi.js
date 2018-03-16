e683b938f5040a3413604668a8f476895c63a8b9
minhnt95 <tienminh.uet@gmail.com>
2018-03-16 15:39:17 +0700

    fix

--- app/api/StaffApi.js
+++ app/api/StaffApi.js
@@ -20,10 +20,8 @@ export function createNewStaff(project_id, staff) {
     return post(`/api/a/staffs`, payload, getByProjectId(project_id));
 }
 
-export function updateStaff(project_id, staff) {
-    const payload = {...staff};
-
-    return put(`/api/a/staffs/${staff._id}`, payload, getByProjectId(project_id));
+export function updateStaff(project_id, staff_id, payload) {
+    return put(`/api/a/staffs/${staff_id}`, payload, getByProjectId(project_id));
 }
 
 export function deleteStaff(project_id, staff_id) {
